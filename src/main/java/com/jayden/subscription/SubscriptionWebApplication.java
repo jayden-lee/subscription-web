da1faf170e732b65092d7c82f2d9a2337d3e29a9
jayden-lee <gwanggi.lee@gmail.com>
2020-12-20 16:13:14 +0900

    Init project

---
+++ src/main/java/com/jayden/subscription/SubscriptionWebApplication.java
@@ -0,0 +1,13 @@
+package com.jayden.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayden/subscription/SubscriptionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayden.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
