b29d2a74de361f67b9b67bf8672349d4ce44043e
jayden-lee <gwanggi.lee@gmail.com>
2020-12-25 18:08:23 +0900

    Add database config

---
+++ src/main/java/com/jayden/subscription/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.jayden.subscription.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
+import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+@Configuration
+@EnableR2dbcRepositories
+public class DatabaseConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get("r2dbc:h2:mem:///subscription?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+    }
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        return initializer;
+    }
+}
